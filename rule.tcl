404bbf0aa38fb6d46769e829f5ba9a15f8e498ff
navreddy36 <navreddy36@gmail.com>
2018-11-14 15:31:08 -0800

    Update rule.tcl

--- rule.tcl
+++ rule.tcl
@@ -9,3 +9,13 @@ when HTTP_REQUEST priority 100 {
     set ATTEMPTING_TO_LOGIN 0
   }
 }
+when HTTP_REQUEST priority 200 timing on {
+  # This will get executed second when a HTTP request state is entered
+  # CPU Metric timing is also enabled for this iRule event block
+  log local0. "Request Event 2 - login state: '$ATTEMPTING_TO_LOGIN'"
+}
+when HTTP_REQUEST timing on {
+  # The priority is not specified so it defaults to 500 and this will get executed last.
+  # CPU metric timing is also enabled for this iRule event block
+  unset ATTEMPTING_TO_LOGIN
+}
