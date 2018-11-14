3d16d19820faf7060f2878ac8ce6a4ccb930f30c
navreddy36 <navreddy36@gmail.com>
2018-11-14 13:03:05 -0800

    Update rule.tcl

--- rule.tcl
+++ rule.tcl
@@ -9,3 +9,10 @@ when HTTP_REQUEST priority 100 {
     set ATTEMPTING_TO_LOGIN 0
   }
 }
+if { [HTTP::header Content-Length] > 0 } then {
+  log local0. "Something's coming..."
+} elseif { [HTTP::uri] contains "foobar" } then {
+  log local0. "The user wants some foobar!"
+} else {
+  log local0. "I don't know what this user wants!"
+}
